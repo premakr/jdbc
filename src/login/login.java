c9205ae05b255ff9a3ffbbc94a95ad3124791ac7
premakr <premakr86@gmail.com>
2019-03-14 17:32:16 -0700

    fdsfzx

---
+++ src/login/login.java
@@ -0,0 +1,17 @@
+package login;
+
+import javax.print.DocFlavor.CHAR_ARRAY;
+
+public class login {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+          String s ="prema";
+          for (int i=0;i<s.length();i++)
+          {
+          System.out.println(s.charAt(i));
+          }
+          System.out.println(s.length());
+	}
+
+}
